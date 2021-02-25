90c48fa529e9d17959676b723f0898d4847ef3d5
Clairette-git <marcletty12@gmail.com>
2021-02-25 19:55:02 +0200

    adding quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    QuotesComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
